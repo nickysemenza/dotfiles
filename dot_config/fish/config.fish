22ef3fecc06c49194826d5b5e7d57c39d85ec16b
Nicky Semenza <nicky@nickysemenza.com>
2021-09-16 15:12:41 -0700

    add some more brew items, customizations, indents

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,5 +4,7 @@ set -Up fish_user_paths ~/.cargo/bin
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 set -x PATH $PATH $HOME/bin
+set -x PATH $PATH /opt/homebrew/bin
 
 fish_add_path /usr/local/sbin
+fish_add_path /opt/homebrew/opt/python@3.9/libexec/bin
